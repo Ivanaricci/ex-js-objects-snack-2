5eecbb812c7a5860c64a302532c14671993fb38d
Ivanaricci <ivana.ricci26@gmail.com>
2025-09-22 19:47:40 +0200

    [UPD] Code Question 3

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ secondBurger.ingredients[0] = "Salad";
 
 console.log(hamburger.ingredients[0]); // Salad
 console.log(secondBurger.ingredients[0]); // Salad
+
+
+//  Code Question 3
+
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+// Quanti oggetti sono stati creati durante l'esecuzione di questo codice? 9
